30b7213b9880b7946d35cd82fb3655bb2efc3ed7
willyhp123 <limwilly0@gmail.com>
2022-06-05 10:32:12 +0700

    update

--- app/payment/controller.js
+++ app/payment/controller.js
@@ -1,5 +1,5 @@
 const Payment = require("./model");
-const { ObjectId } = require("MongoDB");
+const { ObjectId } = require("mongodb");
 
 const index = async (req, res, next) => {
   const { search = "" } = req.query;
